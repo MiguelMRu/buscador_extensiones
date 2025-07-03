936467d501707d97b0d5432e9e0555b6cbc0a9b8
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-03 09:14:26 +0200

    primeros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import extension from './mocks/data.json'
+import logo from './assets/images/logo.svg'
+import sunIcon from './assets/images/icon-sun.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +9,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header'>
+        <img src={logo} alt="Logo of the aplication" />
+        <button><img src={sunIcon} alt=""/></button>
+      </header>
+
+      <main>
+          <header className='main-header'> 
+            <h1>Extension List</h1>
+            <div className='filter-buttoms'>
+              <button>All</button>
+              <button>Active</button>
+              <button>Inactive</button>
+            </div>
+          </header>
+
+      </main>
+        
     </>
   )
 }
