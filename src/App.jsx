11b6a83996d32079ebee7aaa4d4bafa6f4e136d1
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-12 10:28:39 +0200

    añadir componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import { useState } from 'react'
-import extension from './mocks/data.json'
+import { Extensions } from './components/Extensions'
 import logo from './assets/images/logo.svg'
 import sunIcon from './assets/images/icon-sun.svg'
+import { FilterButtoms } from './components/FilterButtoms'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <header className='header'>
         <img src={logo} alt="Logo of the aplication" />
-        <button><img src={sunIcon} alt=""/></button>
+        <button><img src={sunIcon} alt="Change light mode"/></button>
+        
       </header>
 
       <main>
           <header className='main-header'> 
             <h1>Extension List</h1>
-            <div className='filter-buttoms'>
-              <button>All</button>
-              <button>Active</button>
-              <button>Inactive</button>
-            </div>
+            <FilterButtoms/>
           </header>
+
+          
+            <Extensions/>
+          
 
       </main>
         
