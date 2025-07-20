003288fe4447b2283bde8d5b5125ff324877a823
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-20 08:46:56 +0200

    refactorizar codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,16 @@
-import { useState } from 'react'
 import { Extensions  } from './components/Extensions'
 import logo from './assets/images/logo.svg'
-import sunIcon from './assets/images/icon-sun.svg'
-import moonIcon from './assets/images/icon-moon.svg'
 import { FilterButtoms } from './components/FilterButtoms'
-import {extensions as initialExtensions}  from './mocks/data.json'
 import './App.css'
-import { useEffect } from 'react'
+import { useDarkMode } from './hooks/useDarkMode'
+import { useExtensions } from './hooks/useExtensions'
+
 
 function App() {
-  const [extensions, setExtensions] = useState(initialExtensions)
-  // Estado para manejar el filtro de extensiones
-  const [filter, setFilter] = useState('All')
 
-  const [darkMode, setDarkMode] = useState(true)
-
-  const icon = darkMode ? sunIcon : moonIcon
-
-
-  useEffect(() =>{
-    if(!darkMode){
-      document.body.classList.add('light-theme')
-    } else {
-      document.body.classList.remove('light-theme')
-    }
-
-    return () =>{
-      document.body.classList.remove('light-theme')
-    }
-
-  },[darkMode])
-
+  const {handleDarkMode, icon} = useDarkMode()
   
-
-  const handleDarkMode = (event) =>{
-    event.preventDefault()
-    setDarkMode(!darkMode)
-    
-  }
-
-
-  // Función para filtrar las extensiones según el estado
-  const filteredExtensions = (extensions) => {
-    if (filter === 'All')  return extensions
-    return extensions.filter(extension => {
-      if (filter === 'Active') {
-        return extension.isActive
-      } else if (filter === 'Inactive') {
-        return !extension.isActive
-      }
-    })
-  }
-  // funcion para activar o desactivar las extensiones 
-  const toggleActivo = (name) => {
-    setExtensions(preExtenxions => 
-      preExtenxions.map(extension => 
-        extension.name === name ? {...extension, isActive: !extension.isActive} : extension)
-      )
-  }
-
-  const removeExtension = (name) =>{
-
-    setExtensions(preExtenxions => 
-      preExtenxions.filter(extension =>
-        name !== extension.name 
-      )
-    )
-  } 
-
-  // Obtener las extensiones filtradas
-  const displayedExtensions = filteredExtensions(extensions)
+  const { extensions, setFilter, toggleActive, removeExtension } = useExtensions()
 
   return (
     <>
@@ -84,7 +25,7 @@ function App() {
             <h1>Extension List</h1>
             <FilterButtoms changeFilter={setFilter} />
           </header>
-            <Extensions extensions={displayedExtensions}  ontoggleActivo={toggleActivo} onRemove={removeExtension}/>
+            <Extensions extensions={extensions}  ontoggleActivo={toggleActive} onRemove={removeExtension}/>
       </main>
         
     </>
